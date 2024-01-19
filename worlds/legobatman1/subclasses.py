54e9ec1027528a4a7f48e6f88281f774e6e7e200
SnolidIce <82353519+SnolidIce@users.noreply.github.com>
2024-01-18 22:27:15 -0600

    Update subclasses.py

--- worlds/legobatman1/subclasses.py
+++ worlds/legobatman1/subclasses.py
@@ -7,21 +7,21 @@ from .regions import MEGA_SHARDS, REGIONS, SEALS
 from .shop import FIGURINES, PROG_SHOP_ITEMS, SHOP_ITEMS, USEFUL_SHOP_ITEMS
 
 if TYPE_CHECKING:
-    from . import MessengerWorld
+    from . import LegoBatman1World
 
 
-class MessengerRegion(Region):
+class LegoBatman1Region(Region):
     
-    def __init__(self, name: str, world: "MessengerWorld") -> None:
+    def __init__(self, name: str, world: "LegoBatman1World") -> None:
         super().__init__(name, world.player, world.multiworld)
         locations = [loc for loc in REGIONS[self.name]]
-        if self.name == "The Shop":
+        if self.name == "Shop":
             shop_locations = {f"The Shop - {shop_loc}": world.location_name_to_id[f"The Shop - {shop_loc}"]
                               for shop_loc in SHOP_ITEMS}
             self.add_locations(shop_locations, MessengerShopLocation)
         elif self.name == "The Craftsman's Corner":
             self.add_locations({figurine: world.location_name_to_id[figurine] for figurine in FIGURINES},
-                               MessengerLocation)
+                               LegoBatman1Location)
         elif self.name == "Tower HQ":
             locations.append("Money Wrench")
         if world.options.shuffle_seals and self.name in SEALS:
@@ -29,12 +29,12 @@ class MessengerRegion(Region):
         if world.options.shuffle_shards and self.name in MEGA_SHARDS:
             locations += [shard for shard in MEGA_SHARDS[self.name]]
         loc_dict = {loc: world.location_name_to_id.get(loc, None) for loc in locations}
-        self.add_locations(loc_dict, MessengerLocation)
+        self.add_locations(loc_dict, LegoBatman1Location)
         world.multiworld.regions.append(self)
 
 
-class MessengerLocation(Location):
-    game = "The Messenger"
+class LegoBatman1Location(Location):
+    game = "Lego Batman: The Video Game"
 
     def __init__(self, player: int, name: str, loc_id: Optional[int], parent: MessengerRegion) -> None:
         super().__init__(player, name, loc_id, parent)
@@ -42,16 +42,16 @@ class MessengerLocation(Location):
             self.place_locked_item(MessengerItem(name, parent.player, None))
 
 
-class MessengerShopLocation(MessengerLocation):
+class LegoBatman1ShopLocation(LegoBatman1Location):
     @cached_property
     def cost(self) -> int:
-        name = self.name.replace("The Shop - ", "")  # TODO use `remove_prefix` when 3.8 finally gets dropped
-        world = cast("MessengerWorld", self.parent_region.multiworld.worlds[self.player])
+        name = self.name.replace("Shop", "")  # TODO use `remove_prefix` when 3.8 finally gets dropped
+        world = cast("LegoBatman1World", self.parent_region.multiworld.worlds[self.player])
         shop_data = SHOP_ITEMS[name]
 
     def access_rule(self, state: CollectionState) -> bool:
-        world = cast("MessengerWorld", state.multiworld.worlds[self.player])
-        can_afford = state.has("Shards", self.player, min(self.cost, world.total_shards))
+        world = cast("LegoBatman1World", state.multiworld.worlds[self.player])
+        can_afford = state.has("Score 6x", self.player)
         return can_afford
 
 
